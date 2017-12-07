70188235ff682eaeaf7f3775935415d771918d6d
Remmy Clay <remmy.lauren@gmail.com>
2017-12-07 05:54:12 +0000

    automatically backed up by learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -63,7 +63,6 @@ class Owner
   end
 
   def list_pets
-    binding.pry
     string = "I have #{self.pets[:fishes].count} fish, #{self.pets[:dogs].count} dog(s), and #{self.pets[:cats].count} cat(s)."
     string
   end
